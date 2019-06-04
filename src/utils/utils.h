7de7a33f36d31b7228d9d0089ef0f1ea038d4acd
YohanPipereau <ypiperea@cisco.com>
2019-06-04 13:56:29 +0200

    Modify gnmi_to_xpath() to make it work with pipeline-gnmi use of origin field

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -27,9 +27,15 @@ inline string gnmi_to_xpath(const Path& path)
   if (path.elem_size() <= 0)
     return str;
 
+  /* pipeline-gnmi collector puts YANG namespace in origin field */
+  if (!path.origin().empty())
+    str += path.origin() + ":";
+  else // telegraf collector puts namespace in path element
+    str += "/";
+
   //iterate over the list of PathElem of a gNMI path
   for (auto &node : path.elem()) {
-    str += "/" + node.name();
+    str += node.name();
     for (auto key : node.key()) //0 or 1 iteration
       str += "[" + key.first + "=\"" + key.second + "\"]";
   }
